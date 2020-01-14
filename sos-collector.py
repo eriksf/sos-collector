b7636d92cd9be5e2a2fc9de08791db4a6f5290d7
Erik Ferlanti <eferlanti@tacc.utexas.edu>
2020-01-14 11:30:59 -0600

    updated copyright

--- sos-collector.py
+++ sos-collector.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 # Copyright (C) 2017, Kyle Squizzato <ksquizz@gmail.com>
+# Copyright (C) 2020, Erik Ferlanti <eferlanti@tacc.utexas.edu>
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
